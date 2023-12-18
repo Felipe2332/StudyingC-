15df61857f33036587142fd60099ad3f70d30bff
Luis Felipe Barbosa <felipebarbosa22@hotmail.com>
2023-12-18 20:01:25 -0300

    Adicionar arquivos de projeto.

---
+++ TryingForFirstTime/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.Write("How are you?");
+Console.WriteLine("Hey");
+
+
+int x;
+x = 123;
+
+int y = 456;
+
+int z = x + y;
+
+
+double height = 1.81;
+Boolean alive = true;
+char symbol = '@'; //Char só tem ''
+string luckPhrase = "End Is Near";
+
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.WriteLine("Your height is: " + height);
+Console.WriteLine("Are you alive ? " + alive);
+Console.WriteLine("Your symbol is: " + symbol);
+Console.WriteLine("Your favorite phrase is: " + luckPhrase);
+
+Console.WriteLine("Who are you ? /n");
+string name = Console.ReadLine();
+Console .WriteLine("Hello " + name);
+
+
+Console.ReadKey();
+
+
+
+
+
